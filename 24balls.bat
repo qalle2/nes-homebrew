d3550f3871f05dd8d6fca391f93d2c2ee8adb3b2
Kalle <qalle85@gmail.com>
2019-12-15 02:34:26 +0200

    Update 24balls.bat

--- 24balls.bat
+++ 24balls.bat
@@ -1,21 +1,10 @@
 @echo off
-rem WARNING: this batch file DELETES files. Run at own risk.
-
 cls
-if exist 24balls.nes del 24balls.nes
 
-echo === assemble.bat: assembling ===
 asm6f 24balls.asm 24balls.nes
-if errorlevel 1 goto error
+if errorlevel 1 goto end
+
 echo.
-
-echo === assemble.bat: comparing to original file ===
 fc /b ..\..\nes\old-programs\24balls.nes 24balls.nes
-if errorlevel 1 goto error
-
-goto end
-
-:error
-echo === assemble.bat: error detected ===
 
 :end
