9f53fd8bfec5d475cf3456e864ebb8c31a1e4afe
qalle2 <qalle85@gmail.com>
2022-04-21 02:27:23 +0300

    optimize

--- README.md
+++ README.md
@@ -50,8 +50,18 @@ Prints *Hello, World!*.
 ![hello.asm](hello.png)
 
 ### Transgender flag
-Shows a flag.
+Shows the transgender flag.
 Note: this program is heavily optimized for size; it does not represent good programming practice.
-The actual size is 97 bytes (including interrupt vectors and CHR data).
+The actual size is 95 bytes (including interrupt vectors and CHR data).
 
 ![transflag.asm](transflag.png)
+
+The program in hexadecimal:
+```
+a2038e00202c022010fbcad0f88a20f1
+ffa002a2188d0720cad0faa9ff88d0f3
+a93f20f1ffa003b9f8ff8d07208810f7
+a92020f1ffa005b9ebffa2c08d0720ca
+d0fa8810f2a90a8d0120000102000201
+8d06208e06206025210030a1ffebff
+```
--- transflag.asm
+++ transflag.asm
@@ -19,11 +19,10 @@ ppudata     equ $2007
             db %00000001, %00000000  ; NROM mapper, vertical name table mirroring
             pad $0010, $00           ; unused
 
-
-; --- Main program --------------------------------------------------------------------------------
+; --- Start of PRG ROM ----------------------------------------------------------------------------
 
             base $c000          ; end of CPU memory space
-            pad $10000-97, $ff  ; note: set start address manually to as large as possible
+            pad $10000-95, $ff  ; note: set start address manually to as large as possible
 
 reset       ; notes: A, X and Y aren't always zero after soft reset (at least in FCEUX);
             ; we use X as the "zero register" as much as possible
@@ -36,32 +35,29 @@ reset       ; notes: A, X and Y aren't always zero after soft reset (at least in
             dex
             bne -
 
-            stx ppuaddr  ; set up CHR RAM (VRAM $0000-$002f)
-            stx ppuaddr  ; tile 0: color #0 only (16 * 0x00)
-            txa          ; tile 1: color #2 only (8 * 0x00, 8 * 0xff)
-            ldy #2       ; tile 2: color #3 only (16 * 0xff)
---          ldx #24      ; that is, write 24 * 0x00 and 24 * 0xff
--           sta ppudata
+            txa             ; set up CHR RAM (VRAM $0000-$002f)
+            jsr setppuaddr  ; tile 0: color #0 only (16 * 0x00)
+            ldy #2          ; tile 1: color #2 only (8 * 0x00, 8 * 0xff)
+--          ldx #24         ; tile 2: color #3 only (16 * 0xff)
+-           sta ppudata     ; that is, write 24 * 0x00 and 24 * 0xff
             dex
             bne -
             lda #$ff
             dey
             bne --
 
-            lda #$3f       ; set up palette (VRAM $3f00-$3f03)
-            sta ppuaddr
-            stx ppuaddr
+            lda #$3f        ; set up palette (VRAM $3f00-$3f03)
+            jsr setppuaddr
             ldy #3
 -           lda palette,y
             sta ppudata
             dey
             bpl -
 
-            lda #$20     ; set up name & attribute table 0
-            sta ppuaddr  ; each stripe in name table is 6*32 = 192 bytes
-            stx ppuaddr  ; the "6th stripe" actually clears attribute table 0 (and writes garbage
-            ldy #5       ; to the start of name table 1, which is why we use vertical mirroring)
---          lda tiles,y
+            lda #$20        ; set up name & attribute table 0
+            jsr setppuaddr  ; each stripe in name table is 6*32 = 192 bytes
+            ldy #5          ; "6th stripe" actually clears attribute table 0 (and writes garbage
+--          lda tiles,y     ; to start of name table 1, which is why we use vertical mirroring)
             ldx #(6*32)
 -           sta ppudata
             dex
@@ -81,8 +77,14 @@ tiles       hex 00  ; for clearing attribute table 0 / BRK opcode
             hex 02  ; pink
             hex 01  ; blue
 
-            ; interrupt vectors and palette
+setppuaddr  sta ppuaddr
+            stx ppuaddr
+            rts
+
+; --- Palette and interrupt vectors ---------------------------------------------------------------
+
             pad $10000-8, $ff  ; end of CPU memory space
+
 palette     hex 25     ; pink
             hex 21     ; light blue
             hex 00     ; unused color / low  byte of unused NMI vector
