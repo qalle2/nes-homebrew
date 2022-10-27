1dd3182874f7d7aed4799b2ddabb1aefe0faf798
qalle2 <qalle85@gmail.com>
2022-10-27 11:54:19 +0300

    80 chars/line

--- transflag.asm
+++ transflag.asm
@@ -1,9 +1,11 @@
-; Transgender flag (NES, ASM6; tested on Mednafen & FCEUX)
-; Note: this program is heavily optimized for size; it doesn't represent good programming practice.
+; Transgender flag for the NES. Assembles with ASM6.
+; Shows horizontal blue, pink, white, pink and blue stripes.
+; Note: this program is heavily optimized for size; it doesn't represent good
+; programming practice.
 ; Python 3 program to dump last X bytes of PRG ROM:
 ; f=open("transflag.nes","rb");f.seek(-X,2);f.read().hex();f.close()
 
-; --- Constants -----------------------------------------------------------------------------------
+; --- Constants ---------------------------------------------------------------
 
 ; memory-mapped registers
 ppu_ctrl        equ $2000
@@ -12,90 +14,110 @@ ppu_status      equ $2002
 ppu_addr        equ $2006
 ppu_data        equ $2007
 
-; --- iNES header ---------------------------------------------------------------------------------
+; --- iNES header -------------------------------------------------------------
 
                 ; see https://wiki.nesdev.org/w/index.php/INES
                 base $0000
                 db "NES", $1a            ; file id
-                db 1, 0                  ; 16 KiB PRG ROM, 0 KiB CHR ROM (uses CHR RAM)
-                db %00000001, %00000000  ; NROM mapper, vertical name table mirroring
+                db 1, 0                  ; 16 KiB PRG ROM, 8 KiB CHR RAM
+                db %00000001, %00000000  ; NROM mapper, vertical NT mirroring
                 pad $0010, $00           ; unused
 
-; --- Start of PRG ROM ----------------------------------------------------------------------------
+; --- Start of PRG ROM --------------------------------------------------------
 
-                base $c000              ; end of CPU memory space; note: if necessary, set pad
-                pad $10000-95, $ff      ; address manually according to assembler's error message
+                base $c000
 
-reset           ; notes: A, X and Y aren't always zero after soft reset (at least in FCEUX);
-                ; we use X as the "zero register" as much as possible
+                ; end of CPU memory space; note: if the assembler prints an
+                ; error message, adjust the address manually (see the end of
+                ; the program)
+                pad $10000-95, $ff
 
+reset           ; notes:
+                ; - A, X and Y aren't always zero after soft reset (at least
+                ;   in FCEUX)
+                ; - we use X as the "zero register" as much as possible
+
+                ; disable NMI (and show name table 3, which doesn't matter)
                 ldx #%00000011
-                stx ppu_ctrl            ; disable NMI (and show name table 3, which doesn't matter)
+                stx ppu_ctrl
 
--               bit ppu_status          ; wait for start of VBlank three times; also clear X
+                ; wait for start of VBlank three times; also clear X
+-               bit ppu_status
                 bpl -
                 dex
                 bne -
 
-                txa                     ; set up CHR RAM (VRAM $0000-$002f)
-                jsr set_ppu_addr        ; tile 0: color #0 only (16 * $00)
-                ldy #2                  ; tile 1: color #2 only (8 * $00, 8 * $ff)
---              ldx #24                 ; tile 2: color #3 only (16 * $ff)
--               sta ppu_data            ; that is, write 24 * $00 and 24 * $ff
+                ; set up CHR RAM (PPU $0000-$002f):
+                ; - tile 0: color #0 only
+                ; - tile 1: color #2 only
+                ; - tile 2: color #3 only
+                ; - that is, write 24 * $00 and 24 * $ff
+                ;
+                txa
+                jsr set_ppu_addr        ; A*$100 + X -> PPU address
+                ldy #2
+--              ldx #24
+-               sta ppu_data
                 dex
                 bne -
                 lda #$ff
                 dey
                 bne --
 
-                lda #$3f                ; set up palette (VRAM $3f00-$3f03)
-                jsr set_ppu_addr
+                ; set up palette (PPU $3f00-$3f03)
+                lda #$3f
+                jsr set_ppu_addr        ; A*$100 + X -> PPU address
                 ldy #3
 -               lda palette,y
                 sta ppu_data
                 dey
                 bpl -
 
-                lda #$20                ; set up name & attribute table 0
-                jsr set_ppu_addr        ; each stripe in name table is 6*32 = 192 bytes
-                ldy #5                  ; "6th stripe" actually clears attribute table 0 (and
---              lda stripes,y           ; writes garbage to start of name table 1, which is why
-                ldx #(6*32)             ; we use vertical mirroring)
+                ; set up NT0 & AT0:
+                ; - each stripe in NT is 6*32 = 192 bytes
+                ; - the "6th stripe" actually clears AT0 (and writes garbage to
+                ;   the start of NT1, which is why we use vertical NT
+                ;   mirroring)
+                ;
+                lda #$20
+                jsr set_ppu_addr        ; A*$100 + X -> PPU address
+                ldy #5
+--              lda stripes,y
+                ldx #(6*32)
 -               sta ppu_data
                 dex
                 bne -
                 dey
                 bpl --
 
-                lda #%00001010          ; enable background rendering
+                ; enable background rendering
+                lda #%00001010
                 sta ppu_mask
 
-irq             ; an infinite loop (next byte is also BRK)
+irq             ; an infinite loop (the next byte is also BRK)
 
-stripes         hex 00  ; for clearing attribute table 0; also BRK opcode
-                hex 01  ; blue
-                hex 02  ; pink
-                hex 00  ; white
-                hex 02  ; pink
-                hex 01  ; blue
+stripes         ; NT & AT bytes (stored backwards)
+                db 0                    ; AT; also BRK opcode
+                db 1, 2, 0, 2, 1        ; NT
 
-set_ppu_addr    sta ppu_addr
+set_ppu_addr    ; A*$100 + X -> PPU address
+                sta ppu_addr
                 stx ppu_addr
                 rts
 
-; --- Palette and interrupt vectors ---------------------------------------------------------------
+; --- Palette and interrupt vectors -------------------------------------------
 
-                ; note: for code golf reasons, we want the program to be contiguous and don't use
-                ; "pad" here
+                ; note: for code golf reasons, we want the program to be
+                ; contiguous and don't use "pad" here
                 if $ < $10000-8
                     error "please increase 'pad' address at start of PRG ROM"
                 elseif $ > $10000-8
                     error "please decrease 'pad' address at start of PRG ROM"
                 endif
 
-palette         hex 25     ; pink
-                hex 21     ; light blue
-                hex 00     ; unused color / low  byte of unused NMI vector
-                hex 30     ; white        / high byte of unused NMI vector
-                dw reset   ; reset vector
-                dw irq     ; IRQ vector
+palette         ; palette (stored backwards) and NMI vector (unused)
+                hex 25 21               ; pink, light blue
+                hex 00                  ; unused color / low byte of NMI vector
+                hex 30                  ; white / high byte of NMI vector
+
+                dw reset, irq           ; reset/IRQ vector
