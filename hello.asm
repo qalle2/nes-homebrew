779cb22a3b0384e557c7962b0b6b2cd2152ea892
qalle2 <qalle85@gmail.com>
2022-11-10 08:05:01 +0200

    tidy up, 80 chars/line

--- hello.asm
+++ hello.asm
@@ -1,8 +1,21 @@
-; Prints "Hello, World!" (NES, ASM6)
+; An NES/Famicom program that prints "Hello, World!" in dark blue on light pink
+; background.
+; Tested with Nestopia, FCEUX and Mednafen.
+; Assembles with ASM6.
+;
+; Note: The following ASM6-specific directives are used; if you're using
+; another 6502 assembler, look up their equivalents in the manual:
+;     - db X, Y, ...: output bytes X, Y, ...
+;     - dw X, Y, ...: output 2-byte values X, Y, ...; low byte first
+;     - X equ Y: assign value Y to constant X
+;     - base X: set the address of the assembler's internal program counter
+;       to X (do not output any bytes)
+;     - pad X, Y: output byte Y until the assembler's internal program counter
+;       reaches address X
 
-; --- Constants -----------------------------------------------------------------------------------
+; --- Constants ---------------------------------------------------------------
 
-; memory-mapped registers
+; NES memory-mapped registers
 ppu_ctrl        equ $2000
 ppu_mask        equ $2001
 ppu_status      equ $2002
@@ -13,23 +26,23 @@ dmc_freq        equ $4010
 snd_chn         equ $4015
 joypad2         equ $4017
 
-; colors
-bg_color        equ $34  ; background (pink)
-fg_color        equ $02  ; foreground (blue)
+; --- iNES header -------------------------------------------------------------
 
-; --- iNES header ---------------------------------------------------------------------------------
-
-                base $0000               ; see https://wiki.nesdev.org/w/index.php/INES
-                db "NES", $1a            ; file id
+                ; see https://wiki.nesdev.org/w/index.php/INES
+                ;
+                base $0000
+                db $4e, $45, $53, $1a    ; file id ("NES\x1a")
                 db 1, 1                  ; 16 KiB PRG ROM, 8 KiB CHR ROM
-                db %00000000, %00000000  ; NROM mapper, horizontal name table mirroring
+                db %00000000, %00000000  ; NROM mapper, horizontal mirroring
                 pad $0010, $00           ; unused
 
-; --- Main program --------------------------------------------------------------------------------
+; --- Main program ------------------------------------------------------------
 
-                base $c000              ; last 16 KiB of CPU memory space
+                base $c000             ; last 16 KiB of CPU memory space
 
-reset           ; initialize the NES; see https://wiki.nesdev.org/w/index.php/Init_code
+reset           ; initialize the NES;
+                ; see https://wiki.nesdev.org/w/index.php/Init_code
+                ;
                 sei                     ; ignore IRQs
                 cld                     ; disable decimal mode
                 ldx #%01000000
@@ -43,84 +56,134 @@ reset           ; initialize the NES; see https://wiki.nesdev.org/w/index.php/In
                 stx snd_chn             ; disable sound channels
 
                 jsr wait_vbl_start      ; wait for start of next VBlank
-                jsr wait_vbl            ; wait for start of another VBlank
+                jsr wait_vbl_start      ; wait for start of next VBlank
 
-                ldx #$3f                ; set palette (do this while still in VBlank to avoid
-                lda #$00                ; glitches)
-                jsr set_ppu_addr
-                lda #bg_color
-                sta ppu_data
-                lda #fg_color
-                sta ppu_data
-
-                ldx #$20                ; clear 1st Name Table and Attribute Table (4*256 bytes)
+                ; set palette (do this while still in VBlank to avoid glitches)
+                ;
+                ldx #$3f
                 lda #$00
-                jsr set_ppu_addr
+                jsr set_ppu_addr        ; X * 256 + A -> PPU address
+                ;
+                lda #$34                ; background color (light pink)
+                sta ppu_data
+                lda #$02                ; text color (dark blue)
+                sta ppu_data
+
+                ; clear 1st Name Table and Attribute Table (4*256 bytes)
+                ;
+                ldx #$20
+                lda #$00
+                jsr set_ppu_addr        ; X * 256 + A -> PPU address
                 ldy #4
                 tax
--               sta ppu_data
+                ;
+clr_loop        sta ppu_data
                 inx
-                bne -
+                bne clr_loop
                 dey
-                bne -
+                bne clr_loop
 
-                ldx #$20                ; copy text to 1st Name Table (3rd row, 2nd column)
-                lda #$41                ; (X = source index, byte $80...$ff = terminator)
-                jsr set_ppu_addr
-                ldx #0
--               lda text,x
-                bmi +
+                ; copy text to 1st Name Table (3rd row, 2nd column)
+                ;
+                ldx #$20
+                lda #$41
+                jsr set_ppu_addr        ; X * 256 + A -> PPU address
+                ldx #0                  ; source index
+                ;
+copy_loop       lda text,x
+                bmi exit_copy_loop      ; $80-$ff = terminator
                 sta ppu_data
                 inx
-                jmp -
+                jmp copy_loop
 
-+               jsr wait_vbl_start      ; to avoid glitches when enabling rendering
+exit_copy_loop  jsr wait_vbl_start
 
-                lda #$00                ; reset PPU scroll
+                ; reset PPU scroll
+                lda #$00
                 sta ppu_scroll
                 sta ppu_scroll
-                sta ppu_ctrl            ; no NMI; use 1st Pattern & Name Table for background
-                lda #%00001010          ; show background
+
+                ; disable NMI on VBlank; use 1st Pattern & Name Table for
+                ; background
+                lda #%00000000
+                sta ppu_ctrl
+
+                ; show background
+                lda #%00001010
                 sta ppu_mask
 
--               jmp -                   ; infinite loop
+infinite_loop   jmp infinite_loop       ; an infinite loop
 
-; --- Subroutines and arrays ----------------------------------------------------------------------
+; --- Subroutines and arrays --------------------------------------------------
 
-                ; wait_vbl waits until we're in VBlank (which possibly ends very soon);
-                ; wait_vbl_start waits until start of next VBlank instead;
-                ; both subs also reset ppu_scroll/ppu_addr latch
-wait_vbl_start  bit ppu_status          ; clear VBlank flag
-wait_vbl        bit ppu_status          ; wait until PPU indicates we're in VBlank
-                bpl wait_vbl
+wait_vbl_start  ; wait until start of next VBlank
+                ;
+                ; clear VBlank flag (also resets the ppu_scroll/ppu_addr latch)
+                bit ppu_status
+                ; wait until in VBlank
+vbl_wait_loop   bit ppu_status
+                bpl vbl_wait_loop
                 rts
 
-set_ppu_addr    stx ppu_addr            ; set PPU address from X and A
+set_ppu_addr    ; X * 256 + A -> PPU address
+                stx ppu_addr
                 sta ppu_addr
                 rts
 
-text            hex 01 04 05 05 06 08 00 02 06 07 05 03 09  ; "Hello, World!"
-                hex 80                                      ; terminator ($80-$ff)
+text            ; the text to print (tile indexes)
+                db $01, $04, $05, $05, $06, $08, $00  ; "Hello, "
+                db $02, $06, $07, $05, $03, $09       ; "World!"
+                db $80                                ; terminator ($80-$ff)
 
-; --- Interrupt vectors ---------------------------------------------------------------------------
+; --- Interrupt vectors -------------------------------------------------------
 
                 pad $fffa, $ff          ; end of CPU memory space
                 dw $ffff, reset, $ffff  ; NMI (unused), reset, IRQ (unused)
 
-; --- CHR ROM -------------------------------------------------------------------------------------
+; --- CHR ROM -----------------------------------------------------------------
 
                 base $0000
 
-                ; 16 bytes/tile (first 8 bytes for low bitplane, then high; byte = 8*1 pixels)
-                hex  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  ; $00: " "
-                hex  c6 c6 c6 fe c6 c6 c6 00  00 00 00 00 00 00 00 00  ; $01: "H"
-                hex  c6 c6 c6 d6 d6 d6 6c 00  00 00 00 00 00 00 00 00  ; $02: "W"
-                hex  06 06 06 7e c6 ce 76 00  00 00 00 00 00 00 00 00  ; $03: "d"
-                hex  00 00 7c c6 fc c0 7e 00  00 00 00 00 00 00 00 00  ; $04: "e"
-                hex  30 30 30 30 30 30 18 00  00 00 00 00 00 00 00 00  ; $05: "l"
-                hex  00 00 7c c6 c6 c6 7c 00  00 00 00 00 00 00 00 00  ; $06: "o"
-                hex  00 00 de e0 c0 c0 c0 00  00 00 00 00 00 00 00 00  ; $07: "r"
-                hex  00 00 00 00 00 18 18 30  00 00 00 00 00 00 00 00  ; $08: ","
-                hex  18 18 18 18 18 00 18 00  00 00 00 00 00 00 00 00  ; $09: "!"
+                ; 1 byte encodes 8*1 pixels of one bitplane.
+                ;
+                ; tile $00 (" ")
+                db $00, $00, $00, $00, $00, $00, $00, $00  ; low bitplane
+                db $00, $00, $00, $00, $00, $00, $00, $00  ; high bitplane
+                ;
+                ; tile $01 ("H")
+                db $c6, $c6, $c6, $fe, $c6, $c6, $c6, $00  ; low bitplane
+                db $00, $00, $00, $00, $00, $00, $00, $00  ; high bitplane
+                ;
+                ; tile $02 ("W")
+                db $c6, $c6, $c6, $d6, $d6, $d6, $6c, $00  ; low bitplane
+                db $00, $00, $00, $00, $00, $00, $00, $00  ; high bitplane
+                ;
+                ; tile $03 ("d")
+                db $06, $06, $06, $7e, $c6, $ce, $76, $00  ; low bitplane
+                db $00, $00, $00, $00, $00, $00, $00, $00  ; high bitplane
+                ;
+                ; tile $04 ("e")
+                db $00, $00, $7c, $c6, $fc, $c0, $7e, $00  ; low bitplane
+                db $00, $00, $00, $00, $00, $00, $00, $00  ; high bitplane
+                ;
+                ; tile $05 ("l")
+                db $30, $30, $30, $30, $30, $30, $18, $00  ; low bitplane
+                db $00, $00, $00, $00, $00, $00, $00, $00  ; high bitplane
+                ;
+                ; tile $06 ("o")
+                db $00, $00, $7c, $c6, $c6, $c6, $7c, $00  ; low bitplane
+                db $00, $00, $00, $00, $00, $00, $00, $00  ; high bitplane
+                ;
+                ; tile $07 ("r")
+                db $00, $00, $de, $e0, $c0, $c0, $c0, $00  ; low bitplane
+                db $00, $00, $00, $00, $00, $00, $00, $00  ; high bitplane
+                ;
+                ; tile $08 (",")
+                db $00, $00, $00, $00, $00, $18, $18, $30  ; low bitplane
+                db $00, $00, $00, $00, $00, $00, $00, $00  ; high bitplane
+                ;
+                ; tile $09 ("!")
+                db $18, $18, $18, $18, $18, $00, $18, $00  ; low bitplane
+                db $00, $00, $00, $00, $00, $00, $00, $00  ; high bitplane
 
                 pad $2000, $ff
